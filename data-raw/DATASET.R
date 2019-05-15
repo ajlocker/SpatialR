e48731e70d9f37c6c66a010275645433b26a349e
ajlocker <angelina.sweeney@utexas.edu>
2019-05-15 08:56:10 -0500

    dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,35 +1,41 @@
 ## code to prepare `DATASET` dataset goes here
-
-Zipped_SPA_Shape_file <- system.file("extdata", "Zipped_SPA_Shape_file.shp", package = "SpatialR")
-Zipped_SPA_Shape_file <- Zipped_SPA_Shape_file + system.file("extdata", "Zipped_SPA_Shape_file.dbf", package = "SpatialR")
-Zipped_SPA_Shape_file <- Zipped_SPA_Shape_file + system.file("extdata", "Zipped_SPA_Shape_file.prj", package = "SpatialR")
-Zipped_SPA_Shape_file <- Zipped_SPA_Shape_file + system.file("extdata", "Zipped_SPA_Shape_file.shp", package = "SpatialR")
-Zipped_SPA_Shape_file <- Zipped_SPA_Shape_file + system.file("extdata", "Zipped_SPA_Shape_file.shx", package = "SpatialR")
-Zipped_SPA_Shape_file <- Zipped_SPA_Shape_file + system.file("extdata", "Zipped_SPA_Shape_file.xml", package = "SpatialR")
-Zipped_SPA_Shape_file <- Zipped_SPA_Shape_file + system.file("extdata", "data1964al.xy", package = "SpatialR")
-
-DEPARTAMENTO <- system.file("extdata", "DEPARTAMENTO.shp", package = "SpatialR", lib.loc = NULL, mustWork = TRUE)
-DEPARTAMENTO <- DEPARTAMENTO + system.file("extdata", "DEPARTAMENTO.cpg", package = "SpatialR", lib.loc = NULL, mustWork = TRUE)
-DEPARTAMENTO <- DEPARTAMENTO + system.file("extdata", "DEPARTAMENTO.dbf", package = "SpatialR", lib.loc = NULL, mustWork = TRUE)
-DEPARTAMENTO <- DEPARTAMENTO + system.file("extdata", "DEPARTAMENTO.prj", package = "SpatialR", lib.loc = NULL, mustWork = TRUE)
-DEPARTAMENTO <- DEPARTAMENTO + system.file("extdata", "DEPARTAMENTO.sbn", package = "SpatialR", lib.loc = NULL, mustWork = TRUE)
-DEPARTAMENTO <- DEPARTAMENTO + system.file("extdata", "DEPARTAMENTO.sbx", package = "SpatialR", lib.loc = NULL, mustWork = TRUE)
-DEPARTAMENTO <- DEPARTAMENTO + system.file("extdata", "DEPARTAMENTO.shp.xml", package = "SpatialR", lib.loc = NULL, mustWork = TRUE)
-DEPARTAMENTO <- DEPARTAMENTO + system.file("extdata", "DEPARTAMENTO.shx", package = "SpatialR", lib.loc = NULL, mustWork = TRUE)
-
-SitioArqueologico <- system.file("extdata", "SitioArqueologico.shp", package = "SpatialR")
-SitioArqueologico <- SitioArqueologico + system.file("extdata", "SitioArqueologico.dbf", package = "SpatialR")
-SitioArqueologico <- SitioArqueologico + system.file("extdata", "SitioArqueologico.prj", package = "SpatialR")
-SitioArqueologico <- SitioArqueologico + system.file("extdata", "SitioArqueologico.sbn", package = "SpatialR")
-SitioArqueologico <- SitioArqueologico + system.file("extdata", "SitioArqueologico.sbx", package = "SpatialR")
-SitioArqueologico <- SitioArqueologico + system.file("extdata", "SitioArqueologico.shp.xml", package = "SpatialR")
-SitioArqueologico <- SitioArqueologico + system.file("extdata", "SitioArqueologico.shx", package = "SpatialR")
+library(readr)
+library(dplyr)
+library(readxl)
 
 
-demTIN <- system.file("extdata", "demTIN.tif", package = "SpatialR")
+local <- "data-raw/Zipped_SPA_Shape_file.shp"
+if(!file.exists(local)){
+  download.file("https://github.com/ajlocker/SpatialR/blob/master/data-raw/Zipped_SPA_Shape_file.shp", local)
+}
+Zipped_SPA_Shape_file <- ("C:/Users/ajlocker/Desktop/R Geospatial/Arizona/Zipped_SPA_Shape_file.shp")
+Zipped_SPA_Shape_file <- Zipped_SPA_Shape_file + ("C:/Users/ajlocker/Desktop/R Geospatial/Arizona/Zipped_SPA_Shape_file.dbf")
+Zipped_SPA_Shape_file <- Zipped_SPA_Shape_file + ("C:/Users/ajlocker/Desktop/R Geospatial/Arizona/Zipped_SPA_Shape_file.prj")
+Zipped_SPA_Shape_file <- Zipped_SPA_Shape_file + ("C:/Users/ajlocker/Desktop/R Geospatial/Arizona/Zipped_SPA_Shape_file.shp")
+Zipped_SPA_Shape_file <- Zipped_SPA_Shape_file + ("C:/Users/ajlocker/Desktop/R Geospatial/Arizona/Zipped_SPA_Shape_file.shx")
+Zipped_SPA_Shape_file <- Zipped_SPA_Shape_file + ("C:/Users/ajlocker/Desktop/R Geospatial/Arizona/Zipped_SPA_Shape_file.xml")
+Zipped_SPA_Shape_file <- Zipped_SPA_Shape_file + ("C:/Users/ajlocker/Desktop/R Geospatial/Arizona/data1964al.xy")
 
+DEPARTAMENTO <- ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/DEPARTAMENTO.shp")
+DEPARTAMENTO <- DEPARTAMENTO + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/DEPARTAMENTO.cpg")
+DEPARTAMENTO <- DEPARTAMENTO + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/DEPARTAMENTO.dbf")
+DEPARTAMENTO <- DEPARTAMENTO + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/DEPARTAMENTO.prj")
+DEPARTAMENTO <- DEPARTAMENTO + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/DEPARTAMENTO.sbn")
+DEPARTAMENTO <- DEPARTAMENTO + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/DEPARTAMENTO.sbx")
+DEPARTAMENTO <- DEPARTAMENTO + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/DEPARTAMENTO.shp.xml")
+DEPARTAMENTO <- DEPARTAMENTO + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/DEPARTAMENTO.shx")
 
-hsTIN <- system.file("extdata", "hsTIN.tif", package = "SpatialR")
+SitioArqueologico <- ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/SitioArqueologico.shp")
+SitioArqueologico <- SitioArqueologico + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/SitioArqueologico.dbf")
+SitioArqueologico <- SitioArqueologico + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/SitioArqueologico.prj")
+SitioArqueologico <- SitioArqueologico + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/SitioArqueologico.sbn")
+SitioArqueologico <- SitioArqueologico + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/SitioArqueologico.sbx")
+SitioArqueologico <- SitioArqueologico + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/SitioArqueologico.shp.xml")
+SitioArqueologico <- SitioArqueologico + ("C:/Users/ajlocker/Desktop/R Geospatial/Peru/SitioArqueologico.shx")
+
+demTIN <- ("C:/Users/ajlocker/Desktop/R Geospatial/ATX/demTIN.tif")
+
+hsTIN <- ("C:/Users/ajlocker/Desktop/R Geospatial/ATX/hsTIN.tif")
 
 usethis::use_data("Zipped_SPA_Shape_file")
 usethis::use_data("DEPARTAMENTO")
